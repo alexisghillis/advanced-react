9c007b05d1c4151cf7ad1fcf7e0ec5e70a75f182
alexis.ghillis <alexis.ghillis@gmail.com>
2024-08-23 16:30:45 +0300

    last restructure before config of monorepo

---
+++ packages/scss/src/scripts/build.js
@@ -0,0 +1,47 @@
+const sass = require('sass');
+const Path = require('path');
+const fs = require('fs');
+
+const getComponents = () => {
+    let allComponents = []
+
+    const types = ['atoms', 'molecules', 'organisms']
+
+    types.forEach(type => {
+        const path = `src/${type}`
+        const allFiles = require('fs').readdirSync(path).map(file => ({
+            input: `src/${type}/${file}`,
+            output: `src/lib/css/${file.slice(0, -4) + 'css'}`
+        }))
+
+        allComponents = [
+            ...allComponents,
+            ...allFiles
+        ]
+})
+return allComponents;
+}
+
+const compile = (path, fileName) => {
+    try {
+        const result = sass.renderSync({
+            file: path,
+            outFile: fileName
+        });
+
+        fs.writeFileSync(fileName, result.css.toString()); // Write the compiled CSS to the output file
+
+        console.log('Compilation successful');
+        console.log(result.css.toString());
+    } catch (error) {
+        console.error('Compilation failed:', error);
+    }
+}
+
+compile('src/global.scss', 'src/lib/css/global.css');
+
+console.log(getComponents())
+
+getComponents().forEach(component => {
+    compile(component.input, component.output)
+})
