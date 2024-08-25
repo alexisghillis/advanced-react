c51bb84d93c4802750b1a16c8a87abbef5d56131
alexis.ghillis <alexis.ghillis@gmail.com>
2024-08-25 18:49:02 +0300

    add playground pavckage

---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom';
+
+import {Button} from '@ds.e/react'
+
+ReactDOM.createRoot(document.querySelector('#root')).render(
+        <Button label='Example Button' />
+    )
